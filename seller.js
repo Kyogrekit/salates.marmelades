8ee8b2910872932b9e8e1cf4bd449a2dc1803760
just-a-guy150 <dimonmig301@gmail.com>
2025-02-01 14:44:13 +0200

    added seller profile

--- seller.js
+++ seller.js
@@ -6,8 +6,16 @@ console.log(id);
 
 
 const url = `https://my-json-server.typicode.com/Kyogrekit/salates.marmelades/users/${id}`;
+console.log(url);
 
-addmarmalade(url);
+fetch(url)
+.then(async response => {
+    const data = await response.json();
+    console.log(data);
+
+    addmarmalade(data);
+});
+
 function addmarmalade( marmaladeObject ) {
     const {
         id,
@@ -19,19 +27,13 @@ function addmarmalade( marmaladeObject ) {
     const template = `
     <div class="card">
 			<div class="teammate">
-				<img src="https://t3.ftcdn.net/jpg/05/70/71/06/360_F_570710660_Jana1ujcJyQTiT2rIzvfmyXzXamVcby8.jpg" alt="" class="teammate_img">
+				<img src="${img}" alt="" class="teammate_img">
 				<div class="shop_item_info">
-					<p class="name">Денис</p>
-					<p>Главный режисёр</p>
+					<p class="name">${username}</p>
+					<p class="name">${surname}</p>
 				</div>
 			</div>
     </div>
-    
-    
-    
-    
-    
-    
     `;
 
     cardContainer.insertAdjacentHTML( 'beforeend', template );
